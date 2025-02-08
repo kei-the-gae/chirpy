e954037f21c7d7dc52c54fdd9d436a258f802746
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-02-07 17:39:53 -0800

    change metrics and reset endpoints to have admin prefix, change metrics endpoint content type

--- metrics.go
+++ metrics.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+const htmlTemplate = `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf(htmlTemplate, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
