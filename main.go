06ae4e92ecffcedb1d0ef721b59734a6330c032a
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-02-10 00:26:33 -0800

    add delete chirp route with authorization

--- main.go
+++ main.go
@@ -85,6 +85,7 @@ func main() {
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
+	mux.HandleFunc("DELETE /api/chirps/{chirpID}", apiCfg.handlerChirpsDelete)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
